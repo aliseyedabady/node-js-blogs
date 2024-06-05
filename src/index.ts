06bd9b7d3d7ee77a01ce7c154e484811eb1a77b0
Ali Seyed Abady <66261775+aliseyedabady@users.noreply.github.com>
2024-06-05 12:28:58 +0330

    Add dotenv.config() to index.ts && add nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { api } from "./routes/api";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
+console.log(process.env);
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
