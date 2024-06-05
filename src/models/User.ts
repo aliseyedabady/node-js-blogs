dd77c82eb7ec44e26668d003502b889b4cda37fb
Ali Seyed Abady <66261775+aliseyedabady@users.noreply.github.com>
2024-06-05 18:09:11 +0330

    Change User Model

--- src/models/User.ts
+++ src/models/User.ts
@@ -8,6 +8,10 @@ class User extends Model {
   id!: number;
   name!: string;
   email!: string;
+  password!: string;
+  role!: "admin" | "user";
+  created_at!: string;
+  updated_at!: string;
 }
 
 export default User;
