5fb0b5748e82e13c33a9dde2d8769c1a674f0f43
Ali Seyed Abady <66261775+aliseyedabady@users.noreply.github.com>
2024-06-05 11:24:19 +0330

    Add Routes To Application

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { api } from "./routes/api";
 const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+
+app.use(api);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
