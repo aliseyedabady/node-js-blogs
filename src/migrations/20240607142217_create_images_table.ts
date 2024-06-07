fc7ffa4d9311fc3114d53cdd460c8ba3e6464d95
Ali Seyed Abady <66261775+aliseyedabady@users.noreply.github.com>
2024-06-07 17:56:36 +0330

    Add image migration

--- src/migrations/20240607142217_create_images_table.ts
+++ src/migrations/20240607142217_create_images_table.ts
@@ -1,14 +1,16 @@
 import { Knex } from "knex";
 
 exports.up = async (knex: Knex) => {
-  await knex.schema.createTable("categories", table => {
+  await knex.schema.createTable("images", table => {
     table.increments("id").primary();
-    table.string("title").notNullable().unique();
+    table.string("url").notNullable();
+    table.string("type").notNullable();
+    table.integer("type_id").unsigned().notNullable();
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
 };
 
 exports.down = async (knex: Knex) => {
-  await knex.schema.dropTable("categories");
+  await knex.schema.dropTable("images");
 };
